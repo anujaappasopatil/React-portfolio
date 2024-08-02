7b8c18585da442c67471e6cd4829be2c295846b8
Anuja Patil <anujap700@gmail.com>
2024-08-02 22:50:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      // Make sure to add 'aos' to the external array
+      external: ['aos']
+    }
+  }
+});
